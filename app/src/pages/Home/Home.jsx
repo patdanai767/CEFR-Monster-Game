f9e7b4f0d4076ef3e52fd1c266005a7931edd14b
bowvanit <nnubow1304@gmail.com>
2025-02-20 22:27:26 +0700

    home

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
+
+
+
 function Home() {
   return (
-    <div className="flex">
-      <div className="text-[100px]">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque porro
-        repellendus sequi excepturi ut quam sit rerum explicabo ratione autem!
+    <div className="bg-[url(/src/assets/Background-home.png)] h-[100vh] bg-cover">
+      <div className="flex text-center justify-center ">
+        <div className="text-[100px]">
+          <div class="text-[36px] p-[50px] mt-20 text-green-800">
+            <h1
+            >CEFR
+            </h1>
+            <p>MONSTER</p>
+          </div>
+          <div class="mt-90">
+          <botton class="text-[32px] bg-amber-400 p-5 px-8 "
+          >Play
+          </botton>
+          </div>
+        </div>
       </div>
     </div>
   );
