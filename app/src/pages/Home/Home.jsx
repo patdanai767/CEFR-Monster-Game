d5b56dbdf4e71b9cb93cc8601a7bf368282adec4
patdanai767 <pattproc@gmail.com>
2025-02-20 21:18:26 +0700

    feat: fixed mobile interface

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>
-      Hello
+    <div className="flex">
+      <div className="text-[100px]">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque porro
+        repellendus sequi excepturi ut quam sit rerum explicabo ratione autem!
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
