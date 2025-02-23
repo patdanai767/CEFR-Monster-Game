69e69f5fedd57d301914f7e1a0c69c8b2fb2c02b
bowvanit <nnubow1304@gmail.com>
2025-02-23 12:43:10 +0700

    ddd

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -5,18 +5,18 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="bg-[url(/src/assets/Home-Background.jpg)] h-[100vh] bg-cover">
-      <div className="flex text-center justify-center ">
-        <div className="text-[100px]">
-          <div className="text-[36px] p-[50px] mt-15 text-amber-500 ">
+    <div className="bg-[url(/src/assets/Home-Background.jpg)] h-[100vh] bg-cover bg-no-repeat">
+      <div className="flex text-center justify-center font-game ">
+        <div className="text-[50px]">
+          <div className="text-[36px] p-[50px] mt-15 text-amber-500 text-stroke-black">
             <h1 className=""
             >CEFR
             </h1>
             <p>MONSTER</p>
           </div>
-          <div className="mt-90">
+          <div className="mt-80 ">
             <a href="/mode">
-          <botton className="text-[32px] bg-amber-500 p-5 px-8 border-2 border-black"
+          <botton className="text-[26px] bg-amber-500 p-5 px-10  border-2 border-black"
           >Play
           </botton>
           </a>
--- app/src/pages/Mode/Mode.jsx
+++ app/src/pages/Mode/Mode.jsx
@@ -1,30 +1,52 @@
 import React from "react";
+import { Heart } from "lucide-react";
+import { Clock } from "lucide-react";
+import { WalletCards } from "lucide-react";
+
+
+
 
 
 function Mode() {
   return (
-    <div className="bg-[url(/src/assets/Background-home.png)] h-[100vh] bg-cover">
-      <div className="flex text-center justify-center ">
+    <div className="bg-[url(/src/assets/Home-Background.jpg)] h-[100vh] bg-cover">
+      <botton className="inline-flex absolute left-[32px] top-[32px]">
+        <a href="/" className="p-1 rounded border-2 text-black bg-amber-500 hover:text-white hover:bg-blue-600 hover:border-blue-600">
+          <svg className="w-9 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+          </svg>
+
+        </a>
+      </botton>
+      <div className="flex text-center justify-center font-game ">
         <div className="text-[100px]">
-          <div className="text-[36px] p-[50px] mt-20 text-green-800">
+
+
+
+          <div className="text-[42px] p-[50px] mt-27 text-green-200 text-stroke-black">
             <h1
             >Choose
             </h1>
             <p>mode</p>
           </div>
-          <div className="mt-12 flex flex-col ">
-          <botton className="text-[18px] bg-amber-400 p-4 px-8 mx-18"
-          >Heart mode
-          </botton>
-          <botton className="mt-12 text-[18px] bg-amber-400 p-4 px-8 mx-18"
-          >Time attack
-          </botton>
-          <botton className="mt-12 text-[18px] bg-lime-400 p-4 px-8 mx-18"
-          >Flashcard
-          </botton>
-          
-          
+          <div className="mt-8 flex flex-col ">
+            <div className="text-[20px] w-[360px] h-[68px] bg-amber-400 p-4 px-10 mx-18 border-3 flex items-center justify-center"
+            >Heart mode <Heart fill="red" size={30} className="ml-8"/>
+            </div>
+            <botton className="mt-12 text-[20px] w-[360px] h-[68px] bg-amber-400 p-4 px-8 mx-18 border-3 flex items-center justify-center"
+            >Time attack <Clock fill="#C8EDE0" size={30} className="ml-8"/>
+            </botton>
+            <botton className="mt-12 text-[20px] bg-lime-400 p-4 px-8 mx-18 border-3 flex items-center justify-center"
+            >Flashcard <WalletCards  size={30} className="ml-8"/>
+            </botton>
+            
+            
+
           </div>
+          
+                   
+                  
         </div>
       </div>
     </div>
