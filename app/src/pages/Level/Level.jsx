20f7b9c10844aa55b8c38c2d7a200689737945da
patdanai767 <pattproc@gmail.com>
2025-02-24 01:39:04 +0700

    fix: absolute modal and map

--- app/src/pages/Level/Level.jsx
+++ app/src/pages/Level/Level.jsx
@@ -26,22 +26,22 @@ export default function Level() {
   return (
     /*background*/
     <div
-      className="relative w-screen h-screen bg-cover bg-center font-game"
+      className="overflow-hidden bg-no-repeat h-screen bg-cover font-game"
       style={{ backgroundImage: "url('/background-game_2.png')" }}
     >
       {/* ปุ่มย้อนกลับ */}
       <div
         onClick={handleBack}
-        className="absolute top-[32px] left-[32px] bg-[#E29F51] w-[48px] h-[48px] rounded-[4px] border-[2px] bg-contain bg-center content-center justify-items-center"
+        className="absolute top-[4%] left-[8%] bg-[#E29F51] w-[48px] h-[48px] rounded-[4px] border-[2px] bg-contain bg-center content-center justify-items-center"
       >
         <ChevronLeft strokeWidth={1} size={45} />
       </div>
       {isVolumeOn ? (
-        <div className="absolute top-[809px] left-7 bg-[#E29F51] w-[56px] h-[56px] rounded-full border-[2px] content-center justify-items-center">
+        <div className="absolute top-[90%] left-7 bg-[#E29F51] w-[56px] h-[56px] rounded-full border-[2px] content-center justify-items-center">
           <Volume2 strokeWidth={1} size={40} onClick={handleVolume} />
         </div>
       ) : (
-        <div className="absolute top-[809px] left-7 bg-[#E29F51] w-[56px] h-[56px] rounded-full border-[2px] content-center justify-items-center">
+        <div className="absolute top-[90%] left-7 bg-[#E29F51] w-[56px] h-[56px] rounded-full border-[2px] content-center justify-items-center">
           <VolumeOff strokeWidth={1} size={40} onClick={handleVolume} />
         </div>
       )}
