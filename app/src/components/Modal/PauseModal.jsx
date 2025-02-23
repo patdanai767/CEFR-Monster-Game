2e15dd51d37d6c9e5c53c8baaa6df3245836fecb
patdanai767 <pattproc@gmail.com>
2025-02-24 01:14:35 +0700

    fix: adjust positioning of volume controls in PauseModal for better alignment

--- app/src/components/Modal/PauseModal.jsx
+++ app/src/components/Modal/PauseModal.jsx
@@ -21,16 +21,18 @@ export default function PauseModal({ setIsPause, setIsRunning }) {
   };
 
   return (
-      <div className="justify-items-center absolute top-[220px] w-full font-game">
+    <div className="justify-items-center absolute top-[220px] w-full font-game">
+      <div>
         {isVolumeOn ? (
-          <div className="left-42 -top-10 justify-items-center content-center absolute w-[80px] h-[80px] rounded-[100%] border-3 bg-[#E29F51]">
+          <div className="left-[40%] -top-10 justify-items-center content-center absolute w-[80px] h-[80px] rounded-[100%] border-3 bg-[#E29F51]">
             <Volume2 height={45} width={45} onClick={handleVolume} />
           </div>
         ) : (
-          <div className="left-42 -top-10 justify-items-center content-center absolute w-[80px] h-[80px] rounded-[100%] border-3 bg-[#E29F51]">
+          <div className="left-[40%] -top-10 justify-items-center content-center absolute w-[80px] h-[80px] rounded-[100%] border-3 bg-[#C76735]">
             <VolumeOff height={45} width={45} onClick={handleVolume} />
           </div>
         )}
+      </div>
       <div className="rounded-sm w-3/4 border-3 border-black grid overflow-hidden text-center px-8 py-14 gap-7 bg-[#856360] text-[#E29F51] text-stroke-black">
         <div className="text-[32px] text-[#C8EDE0]">Pause</div>
         <div onClick={handleResume}>Resume</div>
